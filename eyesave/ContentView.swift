82317c7d68f785fcbd2e8b64b2922c5e7847971b
Jasper Jakobs <jasper@spl.nl>
2026-03-13 10:19:27 +0100

    Initial Commit

---
+++ eyesave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eyesave
+//
+//  Created by Jasper Jakobs on 13/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eyesave/eyesaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  eyesaveApp.swift
+//  eyesave
+//
+//  Created by Jasper Jakobs on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct eyesaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
